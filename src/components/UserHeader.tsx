36603fe10a153ad36f6b5e25ecb0bd9fd47aea7a
Dewin <dewinspena@gmail.com>
2024-05-06 18:51:01 -0400

    remove gb

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,27 +1,3 @@
-"use client";
-
-import { Label } from "@radix-ui/react-dropdown-menu";
-import { Settings, Rabbit, Bird, Turtle, User } from "lucide-react";
-import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
-import {
-  Drawer,
-  DrawerTrigger,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerDescription,
-} from "~/components/ui/drawer";
-import { Textarea } from "~/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
-import { useSession } from "@clerk/nextjs";
-
 const Header = () => {
   return (
     <header className="top-0 z-10 flex h-[73px] items-center gap-1 border-b bg-background p-8">
